8576a2b75971b83f74e9446c133c8d6ed911a17e
Dicapro657 <sefuca444@gmail.com>
2025-04-21 05:45:43 +0300

    Update accessControl.js

--- frontend/utils/accessControl.js
+++ frontend/utils/accessControl.js
@@ -1,2 +1,2 @@
 
-export const YOUR_SECRET_CODE = "X1r10n_Lax1rim0sa"; // Replace this with your personal secret code
+export const internationel777Aa = "X1r10n_Lax1rim0sa"; // Replace this with your personal secret code
